6182d0f23eab11ee78cf85368d46c906dd65871b
Iraneldf <iraneldf@uci.cu>
2023-03-31 16:49:37 -0400

    primer commit

--- static/js/filter-table.js
+++ static/js/filter-table.js
@@ -1,48 +1,100 @@
 (function ($) {
 
-  $.fn.filterTable = function ( id ) {
+    $.fn.filterTable = function (id) {
 
-    if (!id) {
-      console.error('You must provide filter input ID in options!');
-      return null;
+        if (!id) {
+            console.error('You must provide filter input ID in options!');
+            return null;
+        }
+
+        // var settings = $.extend({
+        //   // These are the defaults.
+        //   id: ''
+        // }, options );
+        let bool;
+        var filterID = id;
+        var table = $(".ubicaciones");
+
+        $(filterID).on("keyup", function () {
+
+            var value = $(this).val().toUpperCase();
+
+            table.find('.provincia-ubicacion').each(function (index) {
+                $row = $(this);
+                $row.find(".nombre").each(function () {
+                    $cell = $(this).text().toUpperCase();
+
+
+                    if ($cell.indexOf(value) > -1) {
+                        bool = true;
+                    } else {
+                        bool = false;
+                    }
+
+                });
+
+
+                if (bool) {
+                    $row.show();
+                } else {
+                    $row.hide();
+                }
+            }); // table each
+
+        }); // search keyup
+
+        return this;
     }
 
-    // var settings = $.extend({
-    //   // These are the defaults.
-    //   id: ''
-    // }, options );
-
-    var filterID = id;
-    var table = $(this);
-    
-    $(filterID).on("keyup", function() {
-      var value = $(this).val().toUpperCase();
-
-      table.find('tr').each(function(index) {
-
-        $row = $(this);
-        // skip the headers from search
-        if ($row.find("th").length > 0) return;
-        let bool = false;
-
-        $row.find("td").each(function() {
-          $cell = $(this).text().toUpperCase();
-          if ($cell.indexOf(value) > -1) {  
-              bool = true;
-          }
-        });
-
-        if (bool) {  
-          $row.show();
-        }
-        else {
-          $row.hide();
-        }
-      }); // table each
-
-    }); // search keyup
-
-    return this;
-  }
-
 })(jQuery)
+
+//
+// (function ($) {
+//
+//     $.fn.filterTable = function (id) {
+//
+//         if (!id) {
+//             console.error('You must provide filter input ID in options!');
+//             return null;
+//         }
+//
+//         // var settings = $.extend({
+//         //   // These are the defaults.
+//         //   id: ''
+//         // }, options );
+//         let bool;
+//         var filterID = id;
+//         var table = $(".caja-productos");
+//         console.log(table);
+//         $(filterID).on("keyup", function () {
+//             console.log("funciona");
+//             var value = $(this).val().toUpperCase();
+//
+//             table.find('.plantilla-producto').each(function (index) {
+//                 $row = $(this);
+//                 $row.find(".nombre").each(function () {
+//                     $cell = $(this).text().toUpperCase();
+//                     console.log($cell,'CELL');
+//
+//                     if ($cell.indexOf(value) > -1) {
+//                         bool = true;
+//                     }else {
+//                         bool = false;
+//                     }
+//                     console.log(bool,"BOOL");
+//                 });
+//
+//
+//                 if (bool) {
+//                     $row.show();
+//                 } else {
+//                     $row.hide();
+//                 }
+//             }); // table each
+//
+//         }); // search keyup
+//
+//         return this;
+//     }
+//
+// })(jQuery)
